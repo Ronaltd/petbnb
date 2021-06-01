dded5750c0a68eb5e961366b6aff867af15a7ce9
Ronalt Batista <ronaltdbatista@gmail.com>
2021-06-01 16:12:26 -0300

    Validations and associations em desenvolvimento

--- app/models/local.rb
+++ app/models/local.rb
@@ -1,3 +1,4 @@
 class Local < ApplicationRecord
   belongs_to :user
+  has_many :bookings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :locals
+  has_many :bookings
+  has_many :locals, through: :bookings
 end
