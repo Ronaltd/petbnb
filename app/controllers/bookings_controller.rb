eab52bddf515f188db162fc0411ef6f9bd2f4c9d
Ronalt Batista <ronaltdbatista@gmail.com>
2021-06-02 15:29:57 -0300

    Controllers and views done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,27 +10,60 @@ class BookingsController < ApplicationController
   
   def create
     @local = Local.find(params[:local_id])
-    @booking = Booking.new
-    @booking.local = @local
-    @booking.user = current_user
-    if @booking.save
-      # PROCURAR SOBRE AVAILABILITY
-      redirect_to booking_path(@booking.local)
+    @booking = Booking.new(booking_params)
+    bookings = @local.bookings
+    overlap = bookings.any? do |booking|
+      (booking.checkin_date..booking.checkout_date).overlaps?(@booking.checkin_date..@booking.checkout_date)
+    end
+    if !overlap
+      @booking.local = @local
+      @booking.user = current_user
+      if @booking.save
+        redirect_to booking_path(@booking)
+      else
+        render :new, notice: "Esta data já está reservada!!"
+      end
     else
-      render :new
+      render :new, notice: "Esta data já está reservada!!"
     end
   end
 
   def edit
-    
+    @booking = Booking.find(params[:id])
   end
 
   def update
-    
+    @booking = Booking.find(params[:id])
+    @local = @booking.local
+    bookings = @local.bookings
+    temp_booking = Booking.new(booking_params)
+    overlap = bookings.any? do |booking|
+      (booking.checkin_date..booking.checkout_date).overlaps?(temp_booking.checkin_date..temp_booking.checkout_date)
+    end
+    if !overlap
+      if @booking.update(booking_params)
+        redirect_to booking_path(@booking), notice: "Successfully Updated"
+      else
+        render :edit, notice: "Esta data já está reservada!!"
+      end
+    else
+      render :edit, notice: "Esta data já está reservada!!"
+    end
+
   end
 
   def destroy
-    
+    @booking = Booking.find(params[:id])
+    @local = @booking.local
+    @booking.destroy
+    redirect_to locals_path(@local), notice: "Successfully Deleted"
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:checkin_date, :checkout_date, :pet_name, :pet_type, :pet_weight)
+  end
+  
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def my_bookings
+    @bookings = current_user.bookings
+  end
+  
 end
