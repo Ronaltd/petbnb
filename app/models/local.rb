8d13fafd868d3d0200a8b1b2f68654346de29dab
Ronalt Batista <ronaltdbatista@gmail.com>
2021-06-03 15:54:02 -0300

    commit to do merge

--- app/models/local.rb
+++ app/models/local.rb
@@ -2,6 +2,9 @@ class Local < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates: 
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :descritpion, presence: true, length: {maximum:300}
+  validates :price, presence: true
 
 end
