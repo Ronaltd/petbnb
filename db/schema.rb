a0e8d5ee670f69d67b64e56f7c0b7a3ecc942534
Ronalt Batista <ronaltdbatista@gmail.com>
2021-06-04 16:45:20 -0300

    final version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_181458) do
+ActiveRecord::Schema.define(version: 2021_06_04_194041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_181458) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_locals_on_user_id"
   end
 
